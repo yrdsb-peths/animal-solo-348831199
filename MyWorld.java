43256195c51c7e86c8835ab05338de1cc1316593
348831199 <348831199@gapps.yrdsb.ca>
2026-04-27 11:18:08 -0400

    Version 1.1 + 1.2

--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,11 @@
 import greenfoot.*;
 
+/**
+ *  The World our hero lives.
+ * 
+ * @author Aidan
+ * @version Ver 1.2 April 2026
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
